d436d073506a5dbc63391208bc7bddf31fd9b3f4
Sesamgamer <73685269+DoCosta@users.noreply.github.com>
2024-03-10 20:47:37 +0100

    Timeout added
    added Timeout to visualize process

--- InsertionSorter.java
+++ InsertionSorter.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class InsertionSorter {
     private static int cachednumber = 0;
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         Random rand = new Random();
         int upperbound = 1000;
         int[] numbers = new int [15];
@@ -24,6 +25,7 @@ public class InsertionSorter {
             numbers[j] = cachednumber;
             
         System.out.println(Arrays.toString(numbers));
+        TimeUnit.SECONDS.sleep(1);
         }        
     }
 }
