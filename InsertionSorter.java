c79e7cde27b60b5c3a0971d55ad4ea23a96925e5
Dominik Costa <73685269+DoCosta@users.noreply.github.com>
2024-03-10 20:39:25 +0100

    Add files via upload

---
+++ InsertionSorter.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class InsertionSorter {
+    private static int cachednumber = 0;
+    public static void main(String[] args){
+        Random rand = new Random();
+        int upperbound = 1000;
+        int[] numbers = new int [15];
+        for(int k = 0; k < numbers.length; k++){
+            numbers[k] = rand.nextInt(upperbound);
+        }
+        
+        System.out.println("Random Numbers:");
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("Insertion Sort:");
+        for( int i = 1; i < numbers.length; i++) {
+            cachednumber = numbers[i];
+            int j = i;
+            while (j > 0 && cachednumber < numbers[j - 1]) {
+                numbers[j] = numbers[j - 1];
+                j--;
+            }
+            numbers[j] = cachednumber;
+            
+        System.out.println(Arrays.toString(numbers));
+        }        
+    }
+}
